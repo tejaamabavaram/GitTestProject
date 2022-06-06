6b9a3e5f8a1466731cc0228118650857e102dee8
Bannu <Bannu@LAPTOP-6O2UISIC>
2022-06-07 00:16:56 +0530

    File 2 deleted qand 3 adeed

--- Abc/src/test/atest3.java
+++ Abc/src/test/atest3.java
@@ -1,9 +1,9 @@
 package test;
 
-public class Test2 {
+public class atest3 {
 
 	public static void main(String[] args) {
-		System.out.println("SUcesful");
+		// TODO Auto-generated method stub
 
 	}
 
